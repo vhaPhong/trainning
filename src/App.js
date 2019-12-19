e6f19445e58aa2090f5340edf308d74e38e4e1b7
phong <phong@example.com>
2019-12-19 14:03:56 +0700

    Create React Form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,142 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Button,Form} from 'react-bootstrap'
 
-function App() {
+
+export default function FormInput (props) {
+  const name = useInput('');
+  const email = useInput('');
+  const phone = useInput('');
+  const birthday = useInput('');
+  const [showInfo, setShowInfo] = useState(false);
+  const nameRegex = /^[A-Za-z0-9_-]{11,50}$/;
+  const phoneRegex = /^[0-9]*$/;
+  const emailRegex = /[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9.]+/;
+  let flag = true;
+  // const dateRegex = /(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d/;
+  
+  function checkNameInput(){
+    if(!nameRegex.test(name.value)){
+      alert('Wrong name!!!')
+      return true;
+    }
+    return false;
+  }
+
+  function checkPhoneNumber(){
+    if(!phoneRegex.test(phone.value)){
+      alert('Wrong number!!!')
+      return true;
+    }
+    return false;
+  }
+
+  function checkEmailInput(){
+    if(!emailRegex.test(email.value)){
+      alert('Wrong email!!!')
+      return true;
+    }
+    return false;
+  }
+
+
+
+  function getDate(){
+      let d = new Date();
+      let date = d.getDate();
+      let year = d.getFullYear();
+      let month = d.getMonth() + 1;
+      let currentDate = `${year}-${month}-${date}`
+      // console.log(Date.parse(day));
+      // console.log(Date.parse(currentDate));
+
+      if(Date.parse(birthday.value) > Date.parse(currentDate)){
+         flag = false;
+      } else {
+         flag = true;
+      }
+      return flag
+}
+
+  function checkDate(x){
+    if(!x){
+      alert('Wrong date time')
+      return true;
+    }
+    return false;
+  }
+
+  function checkInput () {
+    if(checkNameInput() || checkPhoneNumber() || checkEmailInput() || checkDate(flag)) {
+      return false;
+    } else {
+      setShowInfo(true);
+      alert('Success')
+      return true;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div style={{width: '400px',margin:'50px'}}>
+      <Form>
+        <Form.Group>
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control
+            placeholder='Your Name'
+            {...name}
+          />
+          <Form.Text className="text-muted">{(name.value.length >= 11) ? "Name is correct" : "Name isn't correct" }
+          </Form.Text>
+          {showInfo ? 
+          <Form.Text style={{color: 'green'}}>
+            Name: {name.value}
+          </Form.Text> : null}
+          <Form.Label>Telephone Number</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder='Telephone Number'
+            {...phone}
+          />
+          <Form.Text className="text-muted">{((phone.value.length >= 10)&&(phone.value.length <= 11)) ? "Phone is correct" : 'Phone must have 10-11 numbers'}</Form.Text>
+          {showInfo ? 
+          <Form.Text style={{color: 'green'}}>
+            Phone: {phone.value}
+          </Form.Text> : null}
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            placeholder='Your Email'
+            {...email}
+          />
+          <Form.Text className="text-muted">Correct: user1231@gmail.com</Form.Text>
+          {showInfo ? 
+          <Form.Text style={{color: 'green'}}>
+            Email: {email.value}
+          </Form.Text> : null}
+          <Form.Label>Date of Birth</Form.Label>
+          <Form.Control
+            onBlur={getDate}
+            type="date"
+            {...birthday}
+          />
+          {showInfo ? 
+          <Form.Text style={{color: 'green'}}>
+            Birthday: {birthday.value}
+          </Form.Text> : null}
+        </Form.Group>
+        </Form>
+      <Button type="submit" id="myBtn" onClick={checkInput}>Submit</Button>
     </div>
   );
 }
 
-export default App;
+function useInput (initialValue) {
+  const [value,setValue] = useState(initialValue)
+
+  function handleChange(event){
+    setValue(event.target.value)
+  }
+  return {
+    value,
+    onChange: handleChange
+  }
+}
---
+++ src/Infor.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Form} from 'react-bootstrap'
+
+export default function Infor (props) {
+    return (
+    )
+}
